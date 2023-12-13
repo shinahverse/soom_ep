126339c7bc58c6e64e595cae57862ad0dfdc60d2
시나버스 <tutor@shinahverse.kr>
2023-12-13 09:22:42 +0900

    2 사용자 간 채팅하기 2

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,15 +4,18 @@ const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", ()=>{
     console.log("서버와 연결되었습니다.");
-})
+});
 
 socket.addEventListener("message", (message)=>{
-    console.log("서버로부터 받은 메시지: ", message.data);
-})
+    // console.log("서버로부터 받은 메시지: ", message.data);
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+});
 
 socket.addEventListener("close", ()=>{
     console.log("서버와 연결이 끊겼습니다.");
-})
+});
 
 function handleSubmit(event){
     event.preventDefault();
