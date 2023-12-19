b9da6348c3f1e763815af926ba7d4d8c84b8fed8
시나버스 <tutor@shinahverse.kr>
2023-12-19 10:13:25 +0900

    6 채팅룸 만들기 2
    화면에 채팅룸 이름 표시하기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,19 +1,24 @@
 const socket = io();
-
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 const room = document.getElementById("room");
+
 room.hidden = true;
+
+let roomName;
 
 function showRoom(){
     welcome.hidden = true;
     room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `[방이름] ${roomName}`;
 }
 
 function handleSubmitRoom(event){
     event.preventDefault();
     const input = form.querySelector("input");
     socket.emit("enterRoom", input.value, showRoom);
+    roomName = input.value;
     input.value = "";
 };
 
