a3913e9bb2f6ab21fde0fb4e68e46f8d3a7e96d3
시나버스 <tutor@shinahverse.kr>
2023-12-26 10:22:13 +0900

    13.4 화면에 버튼 추가하기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,12 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
+const btnMute = document.getElementById("mute");
+const btnCamera = document.getElementById("camera");
 
 let myStream;
+let muted = false;
+let cameraOff = false;
 
 async function getMedia(){
     try{
@@ -19,3 +23,24 @@ async function getMedia(){
 }
 
 getMedia();
+function handleMute(){
+    if(!muted){
+        btnMute.innerText = "음소거해제";
+        muted = true;
+    }else {
+        btnMute.innerText = "음소거";
+        muted = false;
+    }
+}
+
+function handleCamera() {
+    if (!cameraOff){
+        btnCamera.innerText = "카메라 켜짐";
+        cameraOff = true;
+    }else {
+        btnCamera.innerText = "카메라 꺼짐";
+        cameraOff = false;
+    }
+}
+btnMute.addEventListener("click", handleMute);
+btnCamera.addEventListener("click", handleCamera);
